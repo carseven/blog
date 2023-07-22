b1fe4f201acf892be692ca2020d5ac242719e642
carseven <carles.serra@hotmail.es>
2023-07-23 00:14:13 +0200

    feat: use dracula syntax highlighting theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true,
+    },
+  },
+  site: "https://carles.pro",
   integrations: [
     tailwind(),
     image({
